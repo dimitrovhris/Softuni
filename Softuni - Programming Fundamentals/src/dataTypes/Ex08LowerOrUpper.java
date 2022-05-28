d45223e496dd77ccbc285904f15377c2d7037b55
dimitrovhris <hrisit12@gmail.com>
2022-05-28 20:16:46 +0300

    Solved a Task.

---
+++ Softuni - Programming Fundamentals/src/dataTypes/Ex08LowerOrUpper.java
@@ -0,0 +1,17 @@
+package dataTypes;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Ex08LowerOrUpper {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String letter = sc.nextLine();
+        if(letter.equals(letter.toLowerCase())){
+            System.out.println("lower-case");
+        }
+        else{
+            System.out.println("upper-case");
+        }
+    }
+}
---
+++ Softuni - Programming Fundamentals/src/dataTypesEx/Ex01IntegerOperations.java
@@ -0,0 +1,16 @@
+package dataTypesEx;
+
+import java.util.Scanner;
+
+public class Ex01IntegerOperations {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = Integer.parseInt(sc.nextLine());
+        int num2 = Integer.parseInt(sc.nextLine());
+        int num3 = Integer.parseInt(sc.nextLine());
+        int num4 = Integer.parseInt(sc.nextLine());
+
+        int result = (num1 + num2) / num3 * num4;
+        System.out.println(result);
+    }
+}
