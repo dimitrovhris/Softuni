500d9028951120b0ed9d7e53f2aef86f602be605
Hristiyan Dimitrov <hrisit12@gmail.com>
2022-05-28 18:38:12 +0300

    Add files via upload

---
+++ Softuni - Programming Fundamentals/src/basicSynatx/Basic9.java
@@ -0,0 +1,17 @@
+package basicSynatx;
+
+import java.util.Scanner;
+
+public class Basic9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        int n = Integer.parseInt(sc.nextLine());
+        for (int i = 1; i <= n *2; i+=2){
+            sum+= i;
+            System.out.println(i);
+
+        }
+        System.out.printf("Sum: %d", sum);
+    }
+}
