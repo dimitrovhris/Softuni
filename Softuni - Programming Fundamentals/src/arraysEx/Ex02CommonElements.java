b6b9e8824c67e3f9e04d992daad8254e0941292c
dimitrovhris <hrisit12@gmail.com>
2022-06-03 21:13:40 +0300

    Solved a Task.

---
+++ Softuni - Programming Fundamentals/src/arraysEx/Ex02CommonElements.java
@@ -0,0 +1,22 @@
+package arraysEx;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Ex02CommonElements {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] array1 = sc.nextLine().split(" ");
+        String[] array2 = sc.nextLine().split(" ");
+        int length = Math.min(array1.length, array2.length);
+
+        for(String item : array2){
+            for(String item2: array1){
+                if(item.equals(item2)){
+                    System.out.printf("%s ", item2);
+                }
+            }
+        }
+    }
+}
