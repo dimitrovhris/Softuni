56ab528f7137c8ea549813e3ca3b7f08305c2710
dimitrovhris <hrisit12@gmail.com>
2022-06-11 17:55:26 +0300

    Solved a Task.

--- Softuni - Programming Fundamentals/src/methods/Ex03PrintingTriangle.java
+++ Softuni - Programming Fundamentals/src/methods/Ex03PrintingTriangle.java
@@ -5,6 +5,21 @@ import java.util.Scanner;
 public class Ex03PrintingTriangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        int num = Integer.parseInt(sc.nextLine());
+        printTriangle(num);
+    }
+    public static void printTriangle(int n){
+        for(int i = 1; i <= n; i++){
+            printLine(1, i);
+        }
+        for(int i = n - 1; i >= 1; i--){
+            printLine(1, i);
+        }
+    }
+    public static void printLine(int start, int end){
+        for(int i = start; i <= end; i++){
+            System.out.printf("%d ", i);
+        }
+        System.out.println();
     }
 }
