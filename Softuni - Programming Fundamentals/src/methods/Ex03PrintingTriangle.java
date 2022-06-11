71fc1391f13aafd894e01785c6d6a8ff0d53ea7a
dimitrovhris <hrisit12@gmail.com>
2022-06-11 17:23:14 +0300

    Solved a Task.

---
+++ Softuni - Programming Fundamentals/src/methods/Ex03PrintingTriangle.java
@@ -0,0 +1,10 @@
+package methods;
+
+import java.util.Scanner;
+
+public class Ex03PrintingTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
