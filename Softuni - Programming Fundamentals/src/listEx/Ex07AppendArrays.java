6ba9dc3bd1abd065217aaadd5ac62cc0ba4e7e81
dimitrovhris <hrisit12@gmail.com>
2022-06-19 20:08:25 +0300

    Solved a Task.

--- Softuni - Programming Fundamentals/src/listEx/Ex07AppendArrays.java
+++ Softuni - Programming Fundamentals/src/listEx/Ex07AppendArrays.java
@@ -1,26 +1,19 @@
 package listEx;
 
-import java.util.ArrayList;
+import java.util.*;
 
-import java.util.List;
-import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Ex07AppendArrays {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] arrays = sc.nextLine().split("\\|");
-        List<String> input = new ArrayList<>();
-        for(int k = 0; k <= arrays.length-1; k++){
-            input.add(k, arrays[k]);
-        }
-        String result = "";
-        for(int i = input.size()-1; i >= 0; i--){
-            String[] currentArrayDigits = input.get(i).split(" ");
-            for(int j = 0; j <= currentArrayDigits.length-1; j++){
-                if(!currentArrayDigits[j].equals(""))
-                    result += currentArrayDigits[j] + " ";
-            }
-        }
-        System.out.println(result);
+        List<String> numbers = Arrays.stream(sc.nextLine().split("\\|")).collect(Collectors.toList());
+        Collections.reverse(numbers);
+        System.out.println(numbers.toString()
+                .replace("[", "")
+                .replace("]", "")
+                .replaceAll(",", "")
+                .trim()
+                .replaceAll("\\s+", " "));
     }
 }
