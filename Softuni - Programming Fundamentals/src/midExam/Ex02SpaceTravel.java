a972cec1cf62d6220826c9ccc784d9194dfbc91b
dimitrovhris <hrisit12@gmail.com>
2022-06-29 19:49:57 +0300

    Solved a Task.

--- Softuni - Programming Fundamentals/src/midExam/Ex02SpaceTravel.java
+++ Softuni - Programming Fundamentals/src/midExam/Ex02SpaceTravel.java
@@ -8,51 +8,40 @@ public class Ex02SpaceTravel {
         String[] commands = sc.nextLine().split("\\|\\|");
         int fuel = Integer.parseInt(sc.nextLine());
         int ammunition = Integer.parseInt(sc.nextLine());
-        boolean missionFailed = false;
         for(int i = 0; i <= commands.length-1; i++){
             String command = commands[i].split(" ")[0];
-            switch(command){
-                case "Travel":
-                    int lightYears = Integer.parseInt(commands[i].split(" ")[1]);
-                    int fuelLeft = fuel - lightYears;
-                    if(fuelLeft >= 0){
-                        System.out.printf("The spaceship travelled %d light-years.%n", lightYears);
-                    }
-                    else{
+            if ("Travel".equals(command)) {
+                int lightYears = Integer.parseInt(commands[i].split(" ")[1]);
+                if(lightYears > fuel){
+                    System.out.println("Mission failed.");
+                    break;
+                }
+                else{
+                    System.out.printf("The spaceship travelled %d light-years.%n", lightYears);
+                    fuel -= lightYears;
+                }
+            } else if ("Enemy".equals(command)) {
+                int enemyPoints = Integer.parseInt(commands[i].split(" ")[1]);
+                if (ammunition >= enemyPoints) {
+                    ammunition -= enemyPoints;
+                    System.out.printf("An enemy with %d armour is defeated.%n", enemyPoints);
+                } else {
+                    int runFuel = enemyPoints * 2;
+                    if (fuel - runFuel >= 0) {
+                        System.out.printf("An enemy with %d armour is outmaneuvered.%n", enemyPoints);
+                    } else {
                         System.out.println("Mission failed.");
-                        missionFailed = true;
+                        break;
                     }
-                    break;
-                case "Enemy":
-                    int enemyPoints = Integer.parseInt(commands[i].split(" ")[1]);
-                    if(ammunition >= enemyPoints){
-                        ammunition -= enemyPoints;
-                        System.out.printf("An enemy with %d armour is defeated.%n", enemyPoints);
-                    }
-                    else{
-                        int runFuel = enemyPoints * 2;
-                        if(fuel - runFuel >= 0){
-                            System.out.printf("An enemy with %d armour is outmaneuvered.%n", enemyPoints);
-                        }
-                        else{
-                            System.out.println("Mission failed.");
-                            missionFailed = true;
-                        }
-                    }
-                    break;
-                case "Repair":
-                    int added = Integer.parseInt(commands[i].split(" ")[1]);
-                    fuel += added;
-                    ammunition += added * 2;
-                    System.out.printf("Ammunitions added: %d.%n", added * 2);
-                    System.out.printf("Fuel added: %d.%n", added);
-                    break;
-                case "Titan":
-                    System.out.println("You have reached Titan, all passengers are safe.");
-                    missionFailed = true;
-                    break;
-            }
-            if(missionFailed){
+                }
+            } else if ("Repair".equals(command)) {
+                int added = Integer.parseInt(commands[i].split(" ")[1]);
+                fuel += added;
+                ammunition += added * 2;
+                System.out.printf("Ammunitions added: %d.%n", added * 2);
+                System.out.printf("Fuel added: %d.%n", added);
+            } else if ("Titan".equals(command)) {
+                System.out.println("You have reached Titan, all passengers are safe.");
                 break;
             }
         }
