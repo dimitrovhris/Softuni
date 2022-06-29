78b8647334536a6014700a9ea4a243832a186d18
dimitrovhris <hrisit12@gmail.com>
2022-06-29 20:53:02 +0300

    Solved a Task.

--- Softuni - Programming Fundamentals/src/arraysEx/Ex09ArrayModifier.java
+++ Softuni - Programming Fundamentals/src/arraysEx/Ex09ArrayModifier.java
@@ -6,38 +6,40 @@ import java.util.Scanner;
 public class Ex09ArrayModifier {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] array = Arrays
-                .stream(sc.nextLine().split(" "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
-        String command = sc.nextLine();
-        while(!command.equals("end")){
-            if(command.charAt(0) == 's') {
-                int index1 = Integer.parseInt("" + command.charAt(command.length() - 3));
-                int index2 = Integer.parseInt("" + command.charAt(command.length() - 1));
-                int secondNum = array[index1];
-                array[index1] = array[index2];
-                array[index2] = secondNum;
-            }
-            else if(command.charAt(0) == 'm'){
-                int index1 = Integer.parseInt("" + command.charAt(command.length() - 3));
-                int index2 = Integer.parseInt("" + command.charAt(command.length() - 1));
-                array[index1] = array[index1] * array[index2];
-            }
-            else{
-                for(int i = 0; i<= array.length-1; i++){
-                    array[i] = array[i] - 1;
-                }
-            }
-            command = sc.nextLine();
-        }
-        for(int i = 0; i <= array.length-1; i++){
-            if(i < array.length-1){
-                System.out.printf("%d, ", array[i]);
-            }
-            else{
-                System.out.printf("%d ", array[i]);
-            }
-        }
+       int[] numbers = Arrays.stream(sc.nextLine().split(" "))
+               .mapToInt(Integer::parseInt)
+               .toArray();
+       String input = sc.nextLine();
+       while(!input.equals("end")){
+           String command = input.split(" ")[0];
+           switch(command){
+               case "swap":
+                   int index1 = Integer.parseInt(input.split(" ")[1]);
+                   int num1 = numbers[index1];
+                   int index2 = Integer.parseInt(input.split(" ")[2]);
+                   numbers[index1] = numbers[index2];
+                   numbers[index2] = num1;
+                   break;
+               case "multiply":
+                   int index11 = Integer.parseInt(input.split(" ")[1]);
+                   int index22 = Integer.parseInt(input.split(" ")[2]);
+                   numbers[index11] = numbers[index11] * numbers[index22];
+                   break;
+               case "decrease":
+                   for(int i = 0; i<= numbers.length-1; i++){
+                       numbers[i]--;
+                   }
+                   break;
+           }
+           input = sc.nextLine();
+       }
+       for(int i = 0; i <= numbers.length-1; i++){
+           if(i != numbers.length-1){
+               System.out.printf("%d, ", numbers[i]);
+           }
+           else{
+               System.out.printf("%d", numbers[i]);
+           }
+       }
     }
 }
