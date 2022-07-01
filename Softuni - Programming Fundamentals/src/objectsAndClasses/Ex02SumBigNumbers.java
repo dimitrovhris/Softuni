524da8b333ed0dec4ac0c58faae25b37fd94cf08
dimitrovhris <hrisit12@gmail.com>
2022-07-01 17:04:23 +0300

    Solved a Task.

---
+++ Softuni - Programming Fundamentals/src/objectsAndClasses/Ex02SumBigNumbers.java
@@ -0,0 +1,14 @@
+package objectsAndClasses;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Ex02SumBigNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger num1 = new BigInteger(sc.nextLine());
+        BigInteger num2 = new BigInteger(sc.nextLine());
+        BigInteger sum = num1.add(num2);
+        System.out.println(sum);
+    }
+}
