9989eeaf48ded284f1220f2e60e238323858d71c
dimitrovhris <hrisit12@gmail.com>
2022-07-03 15:41:31 +0300

    Solved a Task.

---
+++ Softuni - Programming Fundamentals/src/objectsAndClassesEx/Ex02Articles/Main.java
@@ -0,0 +1,28 @@
+package objectsAndClassesEx.Ex02Articles;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] inputArticle = sc.nextLine().split(", ");
+        Article article = new Article(inputArticle[0], inputArticle[1], inputArticle[2]);
+        int n = Integer.parseInt(sc.nextLine());
+        for(int i = 1; i <= n; i++){
+            String input = sc.nextLine();
+            String command = input.split(": ")[0];
+            switch(command){
+                case "Edit":
+                    article.edit(input.split(": ")[1]);
+                    break;
+                case "ChangeAuthor":
+                    article.changeAuthor(input.split(": ")[1]);
+                    break;
+                case "Rename":
+                    article.rename(input.split(": ")[1]);
+                    break;
+            }
+        }
+        System.out.println(article);
+    }
+}
