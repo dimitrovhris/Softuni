dddb5879899830cf27520b1f72f14b44899ba9d0
dimitrovhris <hrisit12@gmail.com>
2022-09-28 18:05:44 +0300

    Solved a Task.

---
+++ SoftUni - Java Advanced/src/streamsFilesAndDirectories/Ex01ReadFile.java
@@ -0,0 +1,20 @@
+package streamsFilesAndDirectories;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Ex01ReadFile {
+    public static void main(String[] args) throws IOException {
+        String path = "SoftUni - Java Advanced/resources/input.txt";
+        try(FileInputStream fileStream = new FileInputStream(path)){
+            int oneByte = fileStream.read();
+            while(oneByte >= 0){
+                System.out.printf("%s ", Integer.toBinaryString(oneByte));
+                oneByte = fileStream.read();
+            }
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
