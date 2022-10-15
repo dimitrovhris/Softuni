9ca4e04dc6facca8b80a84d8965862e11bebcbbc
dimitrovhris <hrisit12@gmail.com>
2022-10-15 12:10:32 +0300

    Solved a Task.

--- SoftUni - Java Advanced/src/genericsEx/Main.java
+++ SoftUni - Java Advanced/src/genericsEx/Main.java
@@ -6,9 +6,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = Integer.parseInt(sc.nextLine());
-        Box<String> box = new Box<>();
+        Box<Integer> box = new Box<>();
         while(n -- > 0){
-            String element = sc.nextLine();
+            int element = Integer.parseInt(sc.nextLine());
             box.add(element);
         }
         System.out.println(box);
