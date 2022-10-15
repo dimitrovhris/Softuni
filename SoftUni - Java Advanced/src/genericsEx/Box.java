e7a842ffb8b46f417a9f78f21921738a5f4ff697
dimitrovhris <hrisit12@gmail.com>
2022-10-15 13:27:35 +0300

    Solved a Task.

--- SoftUni - Java Advanced/src/genericsEx/Box.java
+++ SoftUni - Java Advanced/src/genericsEx/Box.java
@@ -2,12 +2,21 @@ package genericsEx;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class Box<T> {
+public class Box<T extends Comparable<T>> {
     private List<T> list;
 
     Box() {
         list = new ArrayList<>();
+    }
+
+    public List<T> getList() {
+        return list;
+    }
+
+    public void setList(List<T> list) {
+        this.list = list;
     }
 
     public void add(T string) {
@@ -20,7 +29,12 @@ public class Box<T> {
         list.set(index1, element2);
         list.set(index2, element1);
     }
-
+    public int countStrings(List<T> list, T compareValue){
+            list = list.stream()
+                    .filter(e-> e.compareTo(compareValue) > 0)
+                    .collect(Collectors.toList());
+            return list.size();
+    }
 
     @Override
     public String toString() {
--- SoftUni - Java Advanced/src/genericsEx/Main.java
+++ SoftUni - Java Advanced/src/genericsEx/Main.java
@@ -6,15 +6,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = Integer.parseInt(sc.nextLine());
-        Box<Integer> box = new Box<>();
+        Box<String> box = new Box<>();
         while(n -- > 0){
-            int element = Integer.parseInt(sc.nextLine());
+            String element = sc.nextLine();
             box.add(element);
         }
-        String[] indexes = sc.nextLine().split(" ");
-        int index1 = Integer.parseInt(indexes[0]);
-        int index2 = Integer.parseInt(indexes[1]);
-        box.swap(index1, index2);
-        System.out.println(box);
+        String compareValue = sc.nextLine();
+        System.out.println(box.countStrings(box.getList(), compareValue));
     }
 }
