2c077b3ff0714e4f8a3d6fc6e20e7315d8bcb309
dimitrovhris <hrisit12@gmail.com>
2022-10-27 20:31:38 +0300

    Solved a Task.

---
+++ SoftUni - Java OOP/src/studentSystem/Main.java
@@ -0,0 +1,19 @@
+package studentSystem;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        StudentSystem studentSystem = new StudentSystem();
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while (!input.equals("Exit")) {
+            String[] data = input.split(" ");
+            if (data[0].equals("Exit")){
+                break;
+            }
+            studentSystem.parseCommand(data);
+            input = sc.nextLine();
+        }
+    }
+}
---
+++ SoftUni - Java OOP/src/studentSystem/StudentSystem.java
@@ -0,0 +1,46 @@
+package studentSystem;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StudentSystem {
+    private Map<String, Student> repo;
+
+    public StudentSystem() {
+        this.repo = new HashMap<>();
+    }
+
+    public Map<String, Student> getRepo() {
+        return this.repo;
+    }
+
+    public void parseCommand(String[] args) {
+
+        if (args[0].equals("Create")) {
+            String name = args[1];
+            int age = Integer.parseInt(args[2]);
+            double grade = Double.parseDouble(args[3]);
+            if (!repo.containsKey(name)) {
+                Student student = new Student(name, age, grade);
+                repo.put(name, student);
+            }
+        } else if (args[0].equals("Show")) {
+            String name = args[1];
+            if (repo.containsKey(name)) {
+                StringBuilder stringBuilder = new StringBuilder();
+                Student student = repo.get(name);
+                String view = String.format("%s is %s years old.", student.getName(), student.getAge());
+                stringBuilder.append(view);
+                if (student.getGrade() >= 5.00) {
+                    stringBuilder.append(" Excellent student.");
+                } else if (student.getGrade() < 5.00 && student.getGrade() >= 3.50) {
+                    stringBuilder.append(" Average student.");
+                } else {
+                    stringBuilder.append(" Very nice person.");
+                }
+
+                System.out.println(stringBuilder.toString());
+            }
+        }
+    }
+}
