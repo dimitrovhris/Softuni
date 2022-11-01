855c3c3a401e4e0ab451105872a8f0eaf9ceb586
dimitrovhris <hrisit12@gmail.com>
2022-11-01 22:47:07 +0200

    Solved a Task.

--- SoftUni - Java OOP/src/Main.java
+++ SoftUni - Java OOP/src/Main.java
@@ -5,21 +5,6 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        double length = Double.parseDouble(reader.readLine());
-        double width = Double.parseDouble(reader.readLine());
-        double height = Double.parseDouble(reader.readLine());
-        boolean invalidData = false;
-        Box box = null;
-        try {
-            box = new Box(length, width, height);
-        } catch (IllegalArgumentException e){
-            System.out.println(e.getMessage());
-            invalidData = true;
-        }
-        if(!invalidData){
-            System.out.printf("Surface Area - %.2f%n", box.calculateSurfaceArea());
-            System.out.printf("Lateral Surface Area - %.2f%n", box.calculateLateralSurfaceArea());
-            System.out.printf("Volume - %.2f%n", box.calculateVolume());
-        }
+
     }
 }
