6bd371307eb067c37e25d10629ec325b3ff573d3
dimitrovhris <hrisit12@gmail.com>
2022-11-06 22:20:12 +0200

    Solved a Task.

--- SoftUni - Java OOP/src/Main.java
+++ SoftUni - Java OOP/src/Main.java
@@ -1,12 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Dog dog = new Dog();
-        dog.eat();
-        dog.bark();
-
-        Cat cat = new Cat();
-        cat.eat();
-        cat.meow();
+        RandomArrayList randomArrayList = new RandomArrayList();
+        randomArrayList.add(1);
+        randomArrayList.add(2);
+        System.out.println(randomArrayList.getRandomElement());
 
     }
 }
---
+++ SoftUni - Java OOP/src/RandomArrayList.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class RandomArrayList extends ArrayList<Object> {
+    public Object getRandomElement(){
+        Random random = new Random();
+        int randomIndex = random.nextInt(0, size());
+        return this.get(randomIndex);
+    }
+}
