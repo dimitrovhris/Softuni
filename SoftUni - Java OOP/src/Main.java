ed3515f35fa460e5d5202c51fcff7e6936d90205
dimitrovhris <hrisit12@gmail.com>
2022-11-06 22:04:35 +0200

    Solved a Task.

--- SoftUni - Java OOP/src/Main.java
+++ SoftUni - Java OOP/src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Dog dog = new Dog();
-        dog.eat();
-        dog.bark();
+        Puppy puppy = new Puppy();
+        puppy.eat();
+        puppy.bark();
+        puppy.weep();
+
     }
 }
