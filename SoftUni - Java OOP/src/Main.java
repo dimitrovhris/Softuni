36e4463eeb3fa77d668efea031e877869460ad91
dimitrovhris <hrisit12@gmail.com>
2022-11-08 18:14:32 +0200

    Solved a Task.

---
+++ SoftUni - Java OOP/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Car sportCar = new Car(98, 200);
+        System.out.println(sportCar.getHorsePower());
+        System.out.println(sportCar.getFuel());
+        System.out.println(sportCar.getFuelConsumption());
+        sportCar.drive(5);
+        System.out.println(sportCar.getFuel());
+    }
+}
