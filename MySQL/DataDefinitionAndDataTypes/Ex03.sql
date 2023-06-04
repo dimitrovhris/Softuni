0afee33ebc2fa98f812409a1ee1e095c1c06a21d
Hristiyan Dimitrov <hrisit12@gmail.com>
2023-06-04 10:49:45 +0300

    Create Ex03.sql
    Solved a problem.

---
+++ MySQL/DataDefinitionAndDataTypes/Ex03.sql
@@ -0,0 +1,9 @@
+INSERT INTO towns(id, name) VALUES
+(1, 'Sofia'),
+(2, 'Plovdiv'),
+(3, 'Varna');
+
+INSERT INTO minions (name, age, town_id) VALUES
+('Kevin', 22, 1),
+('Bob', 15, 3),
+('Steward', null, 2);
