85b7b111f86b20f45e5e39ece83ddbc6a51b4fa9
dimitrovhris <hrisit12@gmail.com>
2023-06-20 20:50:13 +0300

    Solved a problem.

---
+++ Revision/src/fundExam/Ex02TreasureHunt.java
@@ -0,0 +1,11 @@
+package fundExam;
+
+
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+public class Ex02TreasureHunt {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
